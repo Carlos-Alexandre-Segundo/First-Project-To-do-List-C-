f5e609c85460f04a4f41968f730a3119854abc35
Carlos Segundo <segundocarlosalexandre@gmail.com>
2025-11-18 14:06:53 -0400

    initial commit: isso tudo é só um teste

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Tell me, how its going your day ?");
+Console.ReadLine();
+Console.WriteLine("Thank you for sharing! Have a great day ahead!");
+Console.WriteLine("Press any key to exit...");
